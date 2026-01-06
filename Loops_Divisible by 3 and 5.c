cef4026b0761224e9ef88126acca157bae69df84
Nitish Singh <nitish@Nitishs-MacBook-Air.local>
2026-01-06 12:21:16 +0530

    new

---
+++ Loops_Divisible by 3 and 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    for(int i=3;i<=100;++i){
+        
+        if(i%3==0 && i%5==0){
+            printf("%d\n",i);
+        }
+        
+    }
+    return 0;
+}
