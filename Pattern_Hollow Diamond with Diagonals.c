5f2c5ed9b1b589d4446d99fbae98d172fd7bcdff
nitishsingh10 <nitishsingh9hfhs@gmail.com>
2026-01-13 21:20:42 +0530

    new

---
+++ Pattern_Hollow Diamond with Diagonals.c
@@ -0,0 +1,67 @@
+// questions. 
+
+// Write a program that prints a hollow diamond pattern with diagonals for a given integer n, where n represents the number of rows in the top half of the diamond (including the middle row).
+
+// The pattern must be structured as follows:
+
+// Horizontal diagonal: The middle row of the diamond contains astericks with no spaces.
+// Vertical diagonal: The center column of the diamond contains an asterisk in every row.
+// Each subsequent row above or below the middle row reduces the number of astericks symmetrically.
+// The astericks in each row form the hollow diamond shape with visible diagonals.
+// Note: Handle invalid inputs gracefully by displaying an appropriate error message, "Shape Not Possible", if the pattern cannot be drawn by the given input value.
+
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    if(scanf("%d",&n)!=1) return 0;
+    
+    if(n>=2){
+        
+        for(int i = 1 ; i <= n ; ++i){
+            
+            for(int k = 1 ; k <= n-i ; ++k){
+                printf(" ");
+            }
+            
+            for(int j = 1 ; j <= 2*(i) - 1 ; ++j){
+                
+                if((i == 1 || i == n || j == 1 || j == 2*(i)-1 || j == i)){
+                 
+                   printf("*");
+                    
+                }
+                else{
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }
+        
+        for(int i = 2 ; i <= n ; ++i){
+            
+            for(int k = 1 ; k <= i-1 ; ++k){
+                printf(" ");
+            }
+            
+            for(int j = 1 ; j <= 2*(n-i)+1 ; ++j){
+                
+                if(( i == n || j == 1 || j == 2*(n-i)+1 || j == n-i+1)){
+                 
+                   printf("*");
+                    
+                }
+                else{
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }
+        
+    }
+    else{
+        printf("Shape Not Possible");
+    }
+    return 0;
+}
